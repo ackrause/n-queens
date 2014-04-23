5d4afa667601d1b0db8b6db98acc220ad164486d
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-23 16:24:01 -0700

    Naive solution finder for N-Rooks problem

--- src/solvers.js
+++ src/solvers.js
@@ -13,11 +13,40 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+window.generateBoard = function(array) {
+  var board = new Board({n : array.length});
+  for (var i = 0; i < array.length; i++) {
+    board.togglePiece(i, array[i]);
+  }
+  return board;
+};
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+window.findNRooksSolution = function(n, numSoln) {
+  numSoln = numSoln || 0;
+  var solutions = [];
+
+  var genPermutations = function(array) {
+    array = array || [];
+    if (array.length === n) {
+      var testBoard = window.generateBoard(array);
+      if (!testBoard.hasAnyRooksConflicts()) {
+        solutions.push(testBoard);
+      }
+    }
+    else {
+      for (var i = 0; i < n; i++) {
+        if (!_.contains(array, i)) {
+          array.push(i);
+          genPermutations(array);
+          array.pop();
+        }
+      }
+    }
+  };
+  genPermutations();
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solutions[numSoln].rows()));
+  return solutions[numSoln].rows();
 };
 
 
